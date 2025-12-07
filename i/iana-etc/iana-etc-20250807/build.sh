fb4001fbba8cb4c7f8f0764483bc2bd0a7227669
placeja5 <placeja5@fel.cvut.cz>
2025-12-07 13:28:49 +0100

    updated repo

--- i/iana-etc/iana-etc-20250807/build.sh
+++ i/iana-etc/iana-etc-20250807/build.sh
@@ -2,5 +2,8 @@
 # Simple install script for iana-etc
 # Just copy the files to /etc
 
+# Create target directory
+mkdir -p "${DESTDIR}/etc"
+
 install -vm644 services "${DESTDIR}/etc/services"
 install -vm644 protocols "${DESTDIR}/etc/protocols"
